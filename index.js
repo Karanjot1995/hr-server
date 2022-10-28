66fe9a38f8cf25b2f330051bab8d2af0bd3598b8
Kartik Kataria <kartikkataria@Kartiks-MacBook-Air.local>
2022-10-28 16:03:44 -0500

    ...

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const cors = require('cors')
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://hr-management-4a24c.web.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // var whitelist = ['http://localhost:3000', 'http://localhost:3001']
 // var corsOptions = {
 //   origin: function (origin, callback) {
@@ -19,6 +25,11 @@ const cors = require('cors')
 app.get('/', (req, res) => {
     res.send({test:123})
 });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
