4ca1a82e171582b6579ce2e800229de42cec97a6
Kartik Kataria <kartikkataria@Kartiks-MacBook-Air.local>
2022-10-28 14:26:33 -0500

    ...

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const cors = require('cors')
-var whitelist = ['http://localhost:3000']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// var whitelist = ['http://localhost:3000', 'http://localhost:3001']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 // Then pass them to cors:
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send({test:123})
