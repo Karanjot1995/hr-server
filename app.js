f0714e25c4660fcba6d4837c84135fb7dbc6f948
Karanjot Singh <ksingh@megger.net>
2022-12-07 02:06:13 -0600

    dashboard utilization

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cors())
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://hr-management-4a24c.web.app");
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  // res.header("Access-Control-Allow-Origin", "https://hr-management-4a24c.web.app");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -44,7 +44,12 @@ app.post("/api/sign-up", async (req, res) => {
       first_name,
       last_name,
       password: encryptedPassword,
-      hours:[]
+      hours:[],
+      salary:"",
+      dob:"",
+      department:"",
+      designation:"",
+      type:""
     });
 
     // Create token
@@ -64,6 +69,65 @@ app.post("/api/sign-up", async (req, res) => {
     console.log(err);
   }
 });
+
+app.post("/api/user/edit", async (req, res) => {
+  try {
+    // Get user input
+    const details = req.body.employee;
+
+    let user = await User.findOne({ _id:details._id });
+
+    if(user){
+      await User.findOneAndUpdate({_id: details._id}, {
+        first_name: details.first_name,
+        last_name: details.last_name,
+        dob: details.dob,
+        salary:details.salary,
+        department: details.department,
+        designation: details.designation,
+        type: details.type
+      });
+      user = await User.findOne({ _id:details._id });
+      res.status(200).json({user});
+      // return new user
+    }   
+    
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+app.post("/api/user/delete", async (req, res) => {
+  try {
+    // Get user input
+
+    let user = await User.findOne({ _id:req.body._id });
+
+    if(user){
+      await User.deleteOne({_id: req.body._id});
+      res.status(200).json({msg:"deleted"});
+      // return new user
+    }   
+    
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/api/users", async (req, res) => {
+  try {
+    // Get user input
+
+    let users = await User.find({});
+
+    // return new user
+    res.status(200).json({users});
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 
 app.post("/api/login", async (req, res) => {
   try {
@@ -143,11 +207,23 @@ app.get('/api/hours',auth, async(req, res) => {
   if(curr_user){
     let user = await User.findOne({'email' : curr_user.email})
     let hours = await Hours.find({u_id: user._id})
-    console.log(hours)
-
     res.send({hours:hours})
   }
   // let genre_books = 
+})
+
+
+app.get('/api/utilization',auth, async(req, res) => {
+  let curr_user = req.user
+  if(curr_user){
+    let users = await User.find({})
+    for(let i=0;i<users.length;i++){
+      let u = users[i]._doc
+      let timesheet = await Timesheet.find({u_id: users[i]._id})
+      u.timesheet = timesheet
+    }
+    res.send({users})
+  }
 })
 
 
@@ -158,7 +234,6 @@ app.get('/api/timesheet',auth, async(req, res) => {
   if(curr_user){
     let user = await User.findOne({'email' : curr_user.email})
     let timesheet = await Timesheet.find({u_id: user._id})
-    console.log(timesheet)
     res.send({timesheet:timesheet})
   }
   // let genre_books = 
--- model/user.js
+++ model/user.js
@@ -5,8 +5,14 @@ const userSchema = new mongoose.Schema({
   last_name: { type: String, default: null },
   email: { type: String, unique: true },
   password: { type: String },
+  salary: { type: String, default: null },
+  dob: { type: String, default: null },
+  department: { type: String, default: null },
+  designation: { type: String, default: null },
+  type: { type: String, default: null },
   token: { type: String, default: null },
-  hours: { type: Array, default: null  }
+  hours: { type: Array, default: null  },
+  role: { type: String, default: 'user' }
 });
 
 module.exports = mongoose.model("user", userSchema);
