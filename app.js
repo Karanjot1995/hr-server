cfa1172f69e74a649a371d98bcfec5a85067ad5c
Karanjot Singh <ksingh@megger.net>
2022-12-07 02:14:05 -0600

    dashboard utilization

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cors())
 
 app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "https://hr-management-4a24c.web.app");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://hr-management-4a24c.web.app");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -90,8 +90,9 @@ app.post("/api/user/edit", async (req, res) => {
       user = await User.findOne({ _id:details._id });
       res.status(200).json({user});
       // return new user
+    }else{
+      res.status(400).json({msg:"user not found"});
     }   
-    
   } catch (err) {
     console.log(err);
   }
