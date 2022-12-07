bc8789351f160a775fdf0d79a39fb6c6e248df09
Karanjot Singh <ksingh@megger.net>
2022-12-07 12:53:31 -0600

    dashboard utilization

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cors())
 
 app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "https://hr-management-4a24c.web.app");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://hr-management-4a24c.web.app");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
